ed36cfd3ce59cf0b5d153602154162d9810425f3
zhangshijie <zhangshijie@jiandaola.com>
2017-04-11 18:34:39 +0800

    add Fragment

---
+++ app/src/main/java/com/lgd/thesis/lgdthesis/LGDThesisUtils.java
@@ -0,0 +1,9 @@
+package com.lgd.thesis.lgdthesis;
+
+/**
+ * Created by admin on 17/4/11.
+ */
+
+public class LGDThesisUtils {
+    public static final String BmoobApplicationId = "71f596145338c007bf6ba1b5edca5d2f";
+}
---
+++ app/src/main/java/com/lgd/thesis/lgdthesis/application/LGDThesisApplication.java
@@ -0,0 +1,19 @@
+package com.lgd.thesis.lgdthesis.application;
+
+import android.app.Application;
+
+import com.lgd.thesis.lgdthesis.LGDThesisUtils;
+
+
+
+/**
+ * Created by admin on 17/4/11.
+ */
+
+public class LGDThesisApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
